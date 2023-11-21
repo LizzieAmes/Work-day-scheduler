8da087894b8590439ff78231318cac4da5aa20d5
Elizabeth Ames <elizabethames@Elizabeths-MacBook-Pro.local>
2023-11-21 13:37:29 -0700

    created functions using dayjs to change colors of hours

--- Develop/script.js
+++ Develop/script.js
@@ -2,6 +2,44 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
+
+  $("#currentDay").text(dayjs().format("dddd, MMMM D, YYYY"));  
+  $(".saveBtn").on("click", function() {
+    var timeBlockId = $(this).parent().attr("id");
+    var eventText = $(this).siblings(".description").val();
+    localStorage.setItem(timeBlockId, eventText);
+  });
+
+  function updateColors() {
+    var currentHour = dayjs().hour();
+    $(".time-block").each(function () {
+      var blockHour = parseInt($(this).attr("id").split("-")[1]);
+      $(this).removeClass("past present future");
+
+      if (blockHour < currentHour) {
+        $(this).addClass("past");
+      } else if (blockHour === currentHour) {
+          $(this).addCLass("present");
+      } else {
+        $(this).addClass("future");
+      }
+    });
+  }
+
+  updateColors();
+
+  $(".time-block").each(function () {
+    var timeBlockId = $(this).attr("id");
+    var savedEvent = localStorage.getItem(timeBlockId);
+
+    if (savedEvent !== null) {
+      $(this).children(".description").val(savedEvent);
+    }
+  });
+
+  setInterval(updateColors, 60000);
+
+});
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -20,4 +58,3 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
-});
